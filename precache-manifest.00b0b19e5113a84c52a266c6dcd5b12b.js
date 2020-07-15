fbe4f1b5945942fed88338a88f283a3dfb8c31d0
FediGuetat <fediguetat.ytb@gmail.com>
2020-07-15 11:52:07 +0100

    Updates

---
+++ precache-manifest.00b0b19e5113a84c52a266c6dcd5b12b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31149c3bb198b979f55fd05a6654d7a1",
+    "url": "/compo.git/index.html"
+  },
+  {
+    "revision": "132459d0b8b78705b256",
+    "url": "/compo.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b47224e3423fc2326a5f",
+    "url": "/compo.git/static/js/2.a24f1789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/compo.git/static/js/2.a24f1789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132459d0b8b78705b256",
+    "url": "/compo.git/static/js/main.415a36ff.chunk.js"
+  },
+  {
+    "revision": "156133d16d392fff851f",
+    "url": "/compo.git/static/js/runtime-main.b74381fb.js"
+  },
+  {
+    "revision": "b80f2bd0d570fd6e49d08c24de5d115e",
+    "url": "/compo.git/static/media/op.b80f2bd0.jpg"
+  }
+]);
